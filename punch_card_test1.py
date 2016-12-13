122d969bad8c1ed55aa408de8a257b47f2d05a3b
lssxfy123 <liushenshenxfy@126.com>
2016-12-13 09:00:03 +0800

    自动操作浏览器

--- punch_card_test1.py
+++ punch_card_test1.py
@@ -8,16 +8,17 @@ def Splinter(url):
     browser = Browser('chrome')
     browser.visit(url)
     time.sleep(5)
-    with browser.get_iframe('x-URS-iframe') as iframe:
-        browser.find_by_name('email').fill('liushenshenxfy')
-        browser.find_by_name('password').fill('lss19871203')
-        browser.find_by_id('dologin').click()
+    with browser.get_alert() as alert:
+	    print(alert.text)
+        #browser.find_by_name('email').fill('liushenshenxfy')
+        #browser.find_by_name('password').fill('lss19871203')
+        #browser.find_by_id('dologin').click()
     time.sleep(8)
     browser.driver.close()
     #browser.quit()
 
 if __name__ == '__main__':
-    url = 'http://www.126.com'
+    url = 'http://itsoft.hold.founder.com'
     Splinter(url)
 
 
