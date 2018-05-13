1f33470585351b8111dba744bd81da0bb8ac9f9d
lssxfy123 <liushenshenxfy@126.com>
2018-05-13 14:41:02 +0800

    外星人例程

---
+++ example_alien/alien.py
@@ -0,0 +1,25 @@
+# Copyright(C) 2018 刘珅珅
+# Environment: python 3.6.4
+# Date: 2018.5.13
+# 外星人
+import pygame
+from pygame.sprite import Sprite
+
+
+class Alien(Sprite):
+
+    def __init__(self, ai_settings, screen):
+        super(Alien, self).__init__()
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        self.x = float(self.rect.x)
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
