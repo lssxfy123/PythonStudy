7acd2a660c2ba4e9e42b0090cb76bbaf97ed45f1
lssxfy123 <liushenshenxfy@126.com>
2020-10-15 19:40:31 +0800

    查找指定内容并下载相应文档

---
+++ python_word/config.py
@@ -0,0 +1,28 @@
+#encoding=utf-8
+
+import os
+
+# DOC_DIR = "\\\\172.19.56.18\\wordxml$\\doc"
+# OUT_DIR = "\\\\172.19.56.18\\wordxml$\\out"
+
+# DOC_DIR2 = "\\\\172.19.56.18\\wordxml$\\doc2"
+# OUT_DIR2 = "\\\\172.19.56.18\\wordxml$\\out2"
+
+# ftpinfo
+FTP_SERVER = 'ftp3.dc2.fzyun.io'
+FTP_USER = 'shuchang_dev'  
+FTP_PWD = 'Shuchang@2018' 
+FTP_OUT_DIR = "ftp://ftp3.dc2.fzyun.io/bookout"
+FTP_WordDirTestBook = "/lss/test"
+FTP_XmlDirTestBook = "/bookout/lss"
+# FTP_WordDirChapterBook = "/FounderDeployPackage/Autotest/word/ChapterBook"
+FTP_WordDirChapterBook = "/lss/test"
+FTP_XmlDirChapterBook = "/lss/result"
+FTP_WordDirDissertationBook = "/FounderDeployPackage/Autotest/word/DissertationBook"
+FTP_XmlDirDissertationBook = "/bookout/DissertationBook"
+FTP_WordDirStandardBook = "/FounderDeployPackage/Autotest/word/StandardBook"
+FTP_XmlDirStandardBook = "/bookout/StandardBook"
+FTP_AutoTestDir = "/FounderDeployPackage/Autotest"
+# PublishTaskUrl ="http://web.tsengine.dev5.fzyun.io/xmlcpTaskForward/publishTask.do"
+PublishTaskUrl ="http://web.tsengine.test2.fzyun.io/xmlcpTaskForward/publishTask.do"
+RoutingKey = ""
